3870f94cba359f6728ea7eb8bb7b258df0d55b9a
mithas92 <73435940+mithas92@users.noreply.github.com>
2025-04-17 20:52:02 -0500

    added charecter count

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from stats import count_book_words
+from stats import count_book_words, charec_count
 
 def main():
     # frankenstein_book = get_book_text("books/frankenstein.txt")
-    frankenstein_word_count = count_book_words("books/frankenstein.txt")
+    the_book = "books/frankenstein.txt"
+    frankenstein_word_count = count_book_words(the_book)
     print(f"{frankenstein_word_count} words found in the document")
+    frankenstein_char_count = charec_count(the_book)
+    print(frankenstein_char_count)
+
     return
 
 main()
--- stats.py
+++ stats.py
@@ -3,6 +3,21 @@ def get_book_text(book_name):
         file_contents = f.read()
     return file_contents
 
+def charec_count(book_name):
+    with open(book_name) as f:
+        file_contents = f.read()
+    
+    file_contents = file_contents.lower()
+
+    char_dict = {}
+    for c in file_contents:
+        if c in char_dict:
+            char_dict[c] += 1
+        else:
+            char_dict[c] = 1
+
+    return char_dict
+
 def count_book_words(book_name):
     with open(book_name) as f:
         file_contents = f.read()
