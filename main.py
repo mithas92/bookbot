e379bda5fb746467c362bdf60be8bea90859e7d3
mithas92 <73435940+mithas92@users.noreply.github.com>
2025-04-18 11:04:15 -0500

    Report Lesson 1

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-from stats import count_book_words, charec_count
+from stats import count_book_words, charec_count, organize_charec
 
 def main():
     # frankenstein_book = get_book_text("books/frankenstein.txt")
     the_book = "books/frankenstein.txt"
+
     frankenstein_word_count = count_book_words(the_book)
-    print(f"{frankenstein_word_count} words found in the document")
+
     frankenstein_char_count = charec_count(the_book)
-    print(frankenstein_char_count)
+
+    frankenstein_chars_sorted = organize_charec(frankenstein_char_count)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {frankenstein_word_count} total words")
+    print("--------- Character Count -------")
+
+    for charec in frankenstein_chars_sorted:
+        if charec["char"].isalpha():
+            print(f"{charec["char"]}: {charec["value"]}")
+
+    print("============= END ===============")
 
     return
 
--- stats.py
+++ stats.py
@@ -24,3 +24,18 @@ def count_book_words(book_name):
     words_list = file_contents.split()
 
     return len(words_list)
+
+def sort_on(dict_in):
+    return dict_in["value"]
+
+def organize_charec(chars_dict):
+    
+    #Create a sortable list of dictionaries. The sorting item can easily be accessed. 
+    all_chars_list = []
+    for key, value in chars_dict.items():
+        all_chars_list.append({"char": key, "value" :value})
+
+    all_chars_list.sort(reverse=True, key=sort_on)
+
+    return all_chars_list
+
